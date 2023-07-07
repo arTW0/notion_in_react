7a9345cffef50541f5ed4b597261b473cf3cb2c4
arTW0 <arthur.moreira@itec.ufpa.br>
2023-07-07 11:40:15 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export function App() {
+  return (
+    <div className="min-h-screen text-zinc-900 bg-gradient-to-r from-gray-400 via-gray-600 to-blue-800">
+      <div className="bg-white wi[1100px] mx-auto rounded-xl min-h-[200px]"></div>
+    </div>
+  )
+}
