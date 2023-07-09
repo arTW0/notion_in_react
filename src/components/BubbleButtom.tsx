c3be1807384a5db8b32f578fc4bd08e45d603a75
arTW0 <arthur.moreira@itec.ufpa.br>
2023-07-09 17:59:52 -0300

    editing functions

--- src/components/BubbleButtom.tsx
+++ src/components/BubbleButtom.tsx
@@ -8,7 +8,7 @@ export interface BubbleButtonProps extends ComponentProps<'button'> {
 export function BubbleButton(props: BubbleButtonProps) {
   return (
     <button
-      className="p-2 text-zinc-200 text-sm flex irems-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600"
+      className="p-2 text-zinc-200 text-sm flex irems-center gap-1.5 font-medium leading-none hover:text-zinc-50 hover:bg-zinc-600 data-[active=true]:text-violet-400"
       {...props}
     />
   )
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -55,16 +55,28 @@ export function Editor() {
           <RxChatBubble className="w-4 h-4" />
         </BubbleButton>
         <div className="flex items-center">
-          <BubbleButton>
+          <BubbleButton
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            data-active={editor.isActive('bold')}
+          >
             <RxFontBold className="w-4 h-4" />
           </BubbleButton>
-          <BubbleButton>
+          <BubbleButton
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            data-active={editor.isActive('italic')}
+          >
             <RxFontItalic className="w-4 h-4" />
           </BubbleButton>
-          <BubbleButton>
+          <BubbleButton
+            onClick={() => editor.chain().focus().toggleStrike().run()}
+            data-active={editor.isActive('strike')}
+          >
             <RxStrikethrough className="w-4 h-4" />
           </BubbleButton>
-          <BubbleButton>
+          <BubbleButton
+            onClick={() => editor.chain().focus().toggleCode().run()}
+            data-active={editor.isActive('code')}
+          >
             <RxCode className="w-4 h-4" />
           </BubbleButton>
         </div>
